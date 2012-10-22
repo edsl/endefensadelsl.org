eb9ff5e3e64b6d88f1efadcca1c1292ccdad2185
fauno <fauno@endefensadelsl.org>
2012-10-22 14:15:00 -0300

    Generar salidas en distintos formatos con Pandoc
    Usar pandoc para convertir el blog en distintos formatos. Se agrega
    a _config.yml las flags que se le pasan a pandoc (no depende de ruby-pandoc).
    Falta testear y correr el proceso `pandoc`.

---
+++ _plugins/pandoc.rb
@@ -0,0 +1,34 @@
+require 'open3'
+
+module Jekyll
+
+class PandocGenerator < Generator
+    def generate(site)
+       outputs = site.config['pandoc']['outputs']
+       flags  = site.config['pandoc']['flags']
+
+       outputs.each do |output|
+            site.posts.each do |post|
+                filename = File.join(site.config['destination'], post.url).gsub(/\.html$/, ".#{output}")
+
+# Casos especiales para pandoc
+                if ['pdf', 'epub'].include? output 
+                    output_flag = "-o #{filename}"
+                else
+                    output_flag = "-t #{output} -o #{filename}" 
+                end
+
+# Prueba del comando
+                puts "pandoc #{flags} #{output_flag}"
+
+# Correr el comando
+#                Open3::popen3("#{command} #{flags}") do |stdin, stdout, stderr|
+#                    stdin.puts p.content
+#                    stdin.close
+#                end
+           end
+       end
+    
+    end
+end
+end
