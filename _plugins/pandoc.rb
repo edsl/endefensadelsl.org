685311e87aee597a0c5db37820e39a1a06c77f77
Nicolás Reynolds <fauno@kiwwwi.com.ar>
2012-10-23 00:44:51 -0300

    Convertir el HTML con pandoc

--- _plugins/pandoc.rb
+++ _plugins/pandoc.rb
@@ -3,33 +3,48 @@ require 'open3'
 module Jekyll
 
 class PandocGenerator < Generator
-    def generate(site)
-        outputs = site.config['pandoc']['outputs']
-        flags  = site.config['pandoc']['flags']
+  def generate(site)
+    outputs = site.config['pandoc']['outputs']
+    flags  = site.config['pandoc']['flags']
 
-        outputs.each do |output|
-            site.posts.each do |post|
-                filename = File.join(site.config['destination'], post.url).gsub(/\.html$/, ".#{output}")
+    outputs.each do |output|
+      site.posts.each do |post|
+        filename = File.join(site.config['destination'], post.url).gsub(/\.html$/, ".#{output}")
 
 # Casos especiales para pandoc
-                if ['pdf', 'epub'].include? output 
-                    output_flag = "-o #{filename}"
-                else
-                    output_flag = "-t #{output} -o #{filename}" 
-                end
+        if ['pdf', 'epub'].include? output 
+          output_flag = "-o #{filename}"
+        else
+          output_flag = "-t #{output} -o #{filename}" 
+        end
 
 # Prueba del comando
-                puts "pandoc #{flags} #{output_flag}"
+        puts "pandoc #{flags} #{output_flag}"
 
 # Correr el comando
-                Open3::popen3("pandoc #{flags} #{output_flag}") do |stdin, stdout, stderr|
-                    stdin.puts post.content
-                    stdin.close
-                end
-
-            end
+        Open3::popen3("pandoc #{flags} #{output_flag}") do |stdin, stdout, stderr|
+          stdin.puts post.content
+          stdin.close
         end
-
-        end
+      end
     end
+  end
+end
+
+class MarkdownConverter
+  def convert(content)
+    flags  = site.config['pandoc']['flags']
+
+    output = ''
+    Open3::popen3("pandoc #{flags}") do |stdin, stdout, stderr|
+      stdin.puts content
+      stdin.close
+
+      output = stdout.read.strip
+    end
+
+    output
+    
+  end
+end
 end
